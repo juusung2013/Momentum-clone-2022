eeba6716138e84e1348fb9fd0ddaa4ff16483541
juusung2013 <101781036+juusung2013@users.noreply.github.com>
2022-04-05 15:36:53 +0900

    Const, let, var

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const a = 5;
-const b = 2;
+//const, let, var 차이점
 
-const myName = "Nico";
+//const
+//재선언 금지
+const a = b;
+const a = c;
+//(X)
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+//재할당 금지
+const a = b;
+a = c;
+//(X)
+
+//let
+//재선언 금지
+let a = b;
+let a = c;
+//(X)
+
+//재할당 가능
+let a = b;
+a = c;
+//(O)
+
+//var
+//재선언, 재할당 가능
+var a = b;
+var a = c;
+a = d;
+//(O)
+//코드가 길어지면 헷갈리기에 var 사용 안함
+
+//기본은 무조건 const 사용, 변수 수정 필요할 때 let 사용, var는 사용 금지
