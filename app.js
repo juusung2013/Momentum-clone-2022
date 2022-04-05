a52bdbda9abdc17ebf75342ba4cc997940b472aa
juusung2013 <101781036+juusung2013@users.noreply.github.com>
2022-04-05 15:48:32 +0900

    Booleans

--- app.js
+++ app.js
@@ -1,33 +1,5 @@
-//const, let, var 차이점
+//Boolean
 
-//const
-//재선언 금지
-const a = b;
-const a = c;
-//(X)
-
-//재할당 금지
-const a = b;
-a = c;
-//(X)
-
-//let
-//재선언 금지
-let a = b;
-let a = c;
-//(X)
-
-//재할당 가능
-let a = b;
-a = c;
-//(O)
-
-//var
-//재선언, 재할당 가능
-var a = b;
-var a = c;
-a = d;
-//(O)
-//코드가 길어지면 헷갈리기에 var 사용 안함
-
-//기본은 무조건 const 사용, 변수 수정 필요할 때 let 사용, var는 사용 금지
+//null = 값이 없다는 걸 컴퓨터에게 알려주기 위해 의도적으로 채워진 값
+//undefined = 변수는 선언했지만 값을 할당하지 않음
+//ex) let a;
