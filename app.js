08c00945ace9e832087ea93da258605fe6f2d3f3
juusung2013 <101781036+juusung2013@users.noreply.github.com>
2022-04-05 16:58:44 +0900

    Objects

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
+const player = {
+  name: "nico",
+  points: 10,
+  fat: true,
+};
+console.log(player.name);
+console.log(player["name"]);
 
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+console.log(player);
+player.fat = false;
+console.log(player);
 
-// Get Item from Array
-console.log(daysOfWeek);
+//const는 값을 변경할 수 없지만, object인 const 안의 무언가를 변경하는 것은 문제없음.
+//ex)
 
-// Add one more day to the array
-daysOfWeek.push("sun");
+//위와같이 선언했을 때
+//player = false; 불가능
+//player.fat = false; 가능
 
-console.log(daysOfWeek);
+player.lastName = "potato";
+console.log(player);
