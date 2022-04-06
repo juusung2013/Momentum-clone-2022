4f82bf6c3ee81f9cee3325f9fcd221477748adba
juusung2013 <101781036+juusung2013@users.noreply.github.com>
2022-04-06 19:06:11 +0900

    Events - Part 2

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ function handleTitleClick() {
   console.log("title was clicked!");
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is Gone!";
+}
+
 //클릭(event) 하면 handleTitleClick 함수 실행
 //handleTitleClick() 괄호 붙여버리면 클릭 상관없이 바로 실행됨
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+//찾고 싶은 element 검색 방법
+//h1 html element mdn 검색 후, Web APIs 포함된 사이트 들어가기
