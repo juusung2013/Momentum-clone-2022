0e5e8b0546bcd054d1a4c3b84d4a9854e309e270
juusung2013 <101781036+juusung2013@users.noreply.github.com>
2022-04-06 16:47:28 +0900

    Conditionals

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
-//아주 아주 간단한 계산기
+const age = parseInt(prompt("How old are you?"));
 
-const calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-  min: function (a, b) {
-    return a - b;
-  },
-  mul: function (a, b) {
-    return a * b;
-  },
-  div: function (a, b) {
-    return a / b;
-  },
-  squ: function (a, b) {
-    return a ** b;
-  },
-};
-
-const addResult = calculator.add(2, 3);
-const minResult = calculator.min(addResult, 10);
-const mulResult = calculator.mul(10, minResult);
-const divResult = calculator.div(mulResult, addResult);
-const squResult = calculator.squ(divResult, minResult);
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number");
+} else if (age < 18) {
+  console.log("You are too young");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age === 100) {
+  console.log("Wow you are wise");
+} else if (age > 80) {
+  console.log("You can do whatever you want");
+}
